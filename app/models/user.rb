12bda1cfdfcc7fb2bff58d1b94936402e86d43fb
Aleksandra Fedak <fedak.aleksandra@student.uzhnu.edu.ua>
2022-04-24 11:42:57 +0300

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -35,5 +36,34 @@ class User < ActiveRecord::Base
   #Забути користувача
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  #емейл в нижній регістр
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Створює і присвоює активаційний токен і дайджест
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
+  # Повератає true, якщо даний токен співпадає з дайджестом.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  #Активація акаунту
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  #Надсилання листа для активації
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  def setup 
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -9,6 +13,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, params: {user: user_params}
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 
   test "valid signup information" do
@@ -18,8 +24,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                           email: "user@example.com",
                                           password:              "password",
                                           password_confirmation: "password" } } 
-      follow_redirect!
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+
+    #Спроба увійти до активації 
+    log_in_as(user)
+    assert_not is_logged_in?
+
+    #Невалідний активаційний токен
+    get edit_account_activation_path("invalid token")
+    assert_not is_logged_in?
+
+    #Валідний токен, але неправильна адреса пошти
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    
+    #Валідний токен та правильна адреса пошти
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
   end
